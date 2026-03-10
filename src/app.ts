a6d3d52d6c7ac3b3143758ffc610baf40c4c317e
Nitesh Kumar <niteshkjaiswal108@gmail.com>
2026-03-10 15:59:20 +0530

    Add authentication routes to the main application; integrate authRouter for user authentication under /api/v1/auth endpoint.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import { authRouter } from "@/modules/auth/auth.routes.ts";
 
 export const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello from securedeskai");
 });
+
+app.use("/api/v1/auth", authRouter);

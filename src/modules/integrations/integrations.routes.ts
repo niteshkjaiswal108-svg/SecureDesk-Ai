873f436242d7f1579ef7658cd42826db6e65f27c
Nitesh Kumar <niteshkjaiswal108@gmail.com>
2026-03-12 13:45:12 +0530

    Add supported integrations route to return a list of available OAuth providers, enhancing integration functionality.

--- src/modules/integrations/integrations.routes.ts
+++ src/modules/integrations/integrations.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { isAuthenticated } from '@/middlewares/auth0.ts';
-import { connectIntegration, googleCallback } from './integrations.controllers.ts';
+import { connectIntegration, getSupportedIntegrations, googleCallback } from './integrations.controllers.ts';
 
 const integrationRouter = Router();
 
+integrationRouter.get('/supported', isAuthenticated, getSupportedIntegrations)
 integrationRouter.post('/integrations/:provider/connect', isAuthenticated, connectIntegration)
 integrationRouter.get('/integrations/:provider/callback', isAuthenticated, googleCallback)
 

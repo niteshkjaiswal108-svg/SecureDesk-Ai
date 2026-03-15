fda10eb3d64ff077d4f294d776ee9eddfd331be8
Nitesh <niteshkjaiswal108@gmail.com>
2026-03-15 10:22:51 +0530

    Add route for retrieving connected integrations: implement GET endpoint to fetch user-specific integrations, enhancing integration management in the API.

--- src/modules/integrations/integrations.routes.ts
+++ src/modules/integrations/integrations.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { isAuthenticated } from '@/middlewares/auth0.ts';
-import { connectIntegration, getSupportedIntegrations, googleCallback } from './integrations.controllers.ts';
+import { connectIntegration, getConnectedIntegrations, getSupportedIntegrations, googleCallback } from './integrations.controllers.ts';
 
 const integrationRouter = Router();
 
+integrationRouter.get('/', isAuthenticated, getConnectedIntegrations)
 integrationRouter.get('/supported', isAuthenticated, getSupportedIntegrations)
 integrationRouter.post('/:provider/connect', isAuthenticated, connectIntegration)
 integrationRouter.get('/:provider/callback', isAuthenticated, googleCallback)

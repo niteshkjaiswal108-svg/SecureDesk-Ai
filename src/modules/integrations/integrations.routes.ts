94c311024cdfb642274d055adce401d0db498830
Nitesh <niteshkjaiswal108@gmail.com>
2026-03-15 10:23:49 +0530

    Add revoke integration route: implement DELETE endpoint to allow users to revoke integrations, enhancing user control over their connected services.

--- src/modules/integrations/integrations.routes.ts
+++ src/modules/integrations/integrations.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { isAuthenticated } from '@/middlewares/auth0.ts';
-import { connectIntegration, getConnectedIntegrations, getSupportedIntegrations, googleCallback } from './integrations.controllers.ts';
+import { connectIntegration, getConnectedIntegrations, getSupportedIntegrations, googleCallback, revokeIntegration } from './integrations.controllers.ts';
 
 const integrationRouter = Router();
 
@@ -8,5 +8,6 @@ integrationRouter.get('/', isAuthenticated, getConnectedIntegrations)
 integrationRouter.get('/supported', isAuthenticated, getSupportedIntegrations)
 integrationRouter.post('/:provider/connect', isAuthenticated, connectIntegration)
 integrationRouter.get('/:provider/callback', isAuthenticated, googleCallback)
+integrationRouter.delete('/:provider/revoke', isAuthenticated, revokeIntegration)
 
 export default integrationRouter;

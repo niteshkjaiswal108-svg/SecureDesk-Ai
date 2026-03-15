2f7c7f6a8150f9b0fcbcd4c5139d065a88607da0
Nitesh <niteshkjaiswal108@gmail.com>
2026-03-15 12:32:39 +0530

    Update integration callback route to use unified integrationCallback function, enhancing support for multiple providers.

--- src/modules/integrations/integrations.routes.ts
+++ src/modules/integrations/integrations.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { isAuthenticated } from '@/middlewares/auth0.ts';
-import { connectIntegration, getConnectedIntegrations, getSupportedIntegrations, googleCallback, revokeIntegration } from './integrations.controllers.ts';
+import { connectIntegration, getConnectedIntegrations, getSupportedIntegrations, integrationCallback, revokeIntegration } from './integrations.controllers.ts';
 
 const integrationRouter = Router();
 
 integrationRouter.get('/', isAuthenticated, getConnectedIntegrations)
 integrationRouter.get('/supported', isAuthenticated, getSupportedIntegrations)
 integrationRouter.post('/:provider/connect', isAuthenticated, connectIntegration)
-integrationRouter.get('/:provider/callback', isAuthenticated, googleCallback)
+integrationRouter.get('/:provider/callback', isAuthenticated, integrationCallback)
 integrationRouter.delete('/:provider/revoke', isAuthenticated, revokeIntegration)
 
 export default integrationRouter;
